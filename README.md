c8ec1ee066281a0a4f26661c3b401d2bea607024
horace.yeh <horaceyeh.web@gmail.com>
2021-01-10 00:59:09 +0800

    1.ViewModel create 2.data bind 3.command bind 4.xaml viewmodel include

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# WPFBmiExample
+# .NET Framework WPF BMI計算
+
+使用Viewmodel與xaml進行實作，
+DataBind、CommandBind實作在ViewModel中，
+xaml Grid簡易練習
---
+++ WpfApp1/WpfApp1/ViewModels/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using BindingLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp1.ViewModels
+{
+    public class MainViewModel : NotifyPropertyBase
+    {
+        public double _Weight;
+        public double Weight
+        {
+            get { return _Weight; }
+            set { SetProperty(ref _Weight, value); }
+        }
+
+        public double _High;
+        public double High
+        {
+            get { return _High; }
+            set { SetProperty(ref _High, value); }
+        }
+
+        public double _Bmi;
+        public double Bmi
+        {
+            get { return _Bmi; }
+            set { SetProperty(ref _Bmi, value); }
+        }
+
+        private ICommand _BmiCommand;
+
+        public ICommand BmiCommand
+        {
+            get
+            {
+                if (_BmiCommand == null)
+                {
+                    _BmiCommand = new RelayCommand(x => { Bmi = Math.Round(Weight / Math.Pow(High,2), 2);});                  
+                }
+                return _BmiCommand;
+            }
+        }
+    }
+}
