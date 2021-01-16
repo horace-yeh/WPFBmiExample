f9c895bd578ca08f88a12ebb33dc10e3dcf18e1d
horaceyeh <horaceyeh.web@gmail.com>
2021-01-16 09:37:58 +0800

    依照輸出BMI顏色轉換功能

---
+++ WpfApp1/WpfApp1/Converters/NumberColorConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp1.Converters
+{
+    // 注意此 attribute, 可寫可不寫
+    [ValueConversion(typeof(double), typeof(Brush))]
+    public class NumberColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return new SolidColorBrush(Colors.Blue);
+            var v = (double)value;
+            if (v < 18.5)
+            { return new SolidColorBrush(Colors.Blue); }
+            else if (v > 24)
+            { return new SolidColorBrush(Colors.Red); }
+            else
+            { return new SolidColorBrush(Colors.Green); }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
